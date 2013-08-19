4de945488cf51e2547c3839d594054ece067c718
Nelson HOUILLON <houillon.nelson@gmail.com>
2013-08-19 09:59:42 +0200

    initial commit

---
+++ client/client.js
@@ -0,0 +1,545 @@
+/**
+ * "Bible Index" Javascript webclient.
+ * 
+ * Uses WebSockets in order to request verses from a server that handles various
+ * bible's format.
+ * 
+ *  Copyright 2013 Houillon Nelson <houillon.nelson@gmail.com>
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
+ */
+
+/**
+ * Configuration du client WebSocket
+ */
+
+var host = "localhost:8080";
+var ress = "/bible";
+
+/**
+ * Variables et objets globaux
+ */
+
+var filterForm,
+    resultTable,
+    filterBar,
+    referenceSection,
+    launchButton,
+    resetButton,
+    fullBibleReference,
+    referenceErrorSpan,
+    caseInsensitiveCheckbox;
+
+var selectedReferences  = {};
+var displayedVerses = {};
+
+var bookIndex = [
+    "Genèse",
+    "Exode",
+    "Lévitique",
+    "Nombres",
+    "Deutéronome",
+    "Josué",
+    "Juges",
+    "Ruth",
+    "1 Samuel",
+    "2 Samuel",
+    "1 Rois",
+    "2 Rois",
+    "1 Chroniques",
+    "2 Chroniques",
+    "Esdras",
+    "Néhémie",
+    "Esther",
+    "Job",
+    "Psaumes",
+    "Proverbes",
+    "Ecclésiaste",
+    "Cantique des cantiques",
+    "Esaïe",
+    "Jérémie",
+    "Lamentations de Jérémie",
+    "Ezéchiel",
+    "Daniel",
+    "Osée",
+    "Joël",
+    "Amos",
+    "Abdias",
+    "Jonas",
+    "Michée",
+    "Nahum",
+    "Habakuk",
+    "Sophonie",
+    "Aggée",
+    "Zacharie",
+    "Malachie",
+    "Matthieu",
+    "Marc",
+    "Luc",
+    "Jean",
+    "Actes",
+    "Romains",
+    "1 Corinthiens",
+    "2 Corinthiens",
+    "Galates",
+    "Ephésiens",
+    "Philippiens",
+    "Colossiens",
+    "1 Thessaloniciens",
+    "2 Thessaloniciens",
+    "1 Timothée",
+    "2 Timothée",
+    "Tite",
+    "Philémon",
+    "Hébreux",
+    "Jacques",
+    "1 Pierre",
+    "2 Pierre",
+    "1 Jean",
+    "2 Jean",
+    "3 Jean",
+    "Jude",
+    "Apocalypse"
+];
+
+var lastTimestampReceived = null;
+
+/*
+ * Récupère divers noeuds HTML dans des variables globales.
+ * Amorce des écouteurs d'évènement.
+ */
+
+function init()
+{
+    // get
+    filterForm = document.forms["filtre"];
+    resultTable = document.getElementById("resultats_recherche");
+    launchButton = document.getElementById("lancer");
+    resetButton = document.getElementById("effacer");
+    filterBar = document.getElementById("filtre_reference");
+    referenceSection = document.getElementById("liste_reference");
+    fullBibleReference = document.getElementById("bible_entiere");
+    referenceErrorSpan = document.getElementById("reference_error");
+    caseInsensitiveCheckbox = document.getElementById("filtre_case");
+    // action
+    filterForm.addEventListener("submit", enu, false);
+    filterForm.addEventListener("submit", requestServer, false);
+    filterBar.addEventListener("keyup", handleFilterBarKeyUp, false);
+    filterBar.addEventListener("keydown", handleFilterBarKeyDown, false);
+    resetButton.addEventListener("click", resetUI, false);
+    // WS
+    connectToServer();
+}
+
+document.addEventListener("DOMContentLoaded", init, false);
+
+// permet de filtrer les éléments nuls avec la méthode "Array.filter"
+function ret(e) { return e; }
+function enu(e) { e.preventDefault(); }
+
+/**
+ * Gestion de l'UI
+ */
+
+/*
+ * Liste des références sélectionnées
+ */
+
+function addReferenceToList(reference)
+{
+    var ctn = document.createElement("span"),
+        del = document.createElement("span");
+    ctn.classList.add("reference");
+    var s = reference.serialize();
+    ctn.appendChild(document.createTextNode(s));
+    del.classList.add("delete");
+    del.addEventListener("click", removeReferenceFromList, false);
+    ctn.appendChild(del);
+    del.reference = s;
+    referenceSection.appendChild(ctn);
+    // sauvegarde la référence dans un dictionnaire
+    selectedReferences[s] = ctn;
+    // masque la référence globale
+    fullBibleReference.classList.add("gone");
+    toggleResetButton();
+}
+
+function removeReferenceFromList(e)
+{
+    delete selectedReferences[e.target.reference];
+    referenceSection.removeChild(e.target.parentNode);
+    if (referenceSection.childElementCount == 1) {
+        fullBibleReference.classList.remove("gone");
+    }
+    toggleResetButton();
+}
+
+function cleanReferenceList()
+{
+    selectedReferences = {};
+    while (referenceSection.firstChild) {
+        referenceSection.removeChild(referenceSection.firstChild);
+    }
+    fullBibleReference.classList.remove("gone");
+    toggleResetButton();
+}
+
+/*
+ * Affichage des références ayant correspondu à la recherche
+ */
+
+function displayVerse(ref, verse)
+{
+    var tr  = document.createElement("tr");
+    var td1 = document.createElement("td"),
+        td2 = document.createElement("td");
+    var tbody = resultTable.tBodies[0];
+    td1.appendChild(document.createTextNode(ref));
+    td2.appendChild(document.createTextNode(verse));
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tbody.appendChild(tr);
+    displayedVerses[ref] = verse;
+    resultTable.classList.remove("gone");
+    toggleResetButton();
+}
+
+function cleanDisplayedVerses()
+{
+    displayedVerses = {};
+    var tbody = resultTable.tBodies[0];
+    while (tbody.childElementCount > 1) {
+        tbody.removeChild(tbody.lastChild);
+    }
+    toggleResetButton();
+    resultTable.classList.add("gone");
+}
+
+function resetUI()
+{
+    cleanDisplayedVerses();
+    cleanReferenceList();
+    filterForm.reset();
+}
+
+/*
+ * Saisie intelligente des références
+ */
+
+function handleFilterBarKeyUp(e)
+{
+    // Backspace
+    if (e.keyCode == 8) {
+        // Éffacement complet du champs
+        if (!filterBar.value) {
+            referenceErrorSpan.classList.remove("error");
+        }
+    }
+}
+
+function handleFilterBarKeyDown(e)
+{
+    var s = filterBar.value;
+    // Ajout de la référence
+    if (e.keyIdentifier == "Enter") {
+        var ref = new Reference(s);
+        if (ref.parse()) {
+            addReferenceToList(ref);
+            filterBar.value = null;
+            referenceErrorSpan.classList.remove("error");
+        } else {
+            signalReferenceError();
+        }
+        e.preventDefault();
+    }
+    // Prédiction
+    else {
+        suggestBook(s);
+    }
+}
+
+/*
+ * Détermine quel livre biblique est en train d'être ciblé au clavier
+ */
+
+function suggestBook(input)
+{
+    // TODO
+}
+
+/*
+ * Affiche l'avertissement de référence eronnée
+ */
+
+function signalReferenceError()
+{
+    referenceErrorSpan.classList.add("error");
+}
+
+/*
+ * Soumission du formulaire
+ */
+
+function isFormSubmitable()
+{
+    if (s && s.readyState == WebSocket.OPEN) {
+        return true;
+    }
+    return false;
+}
+
+function countProperties(obj)
+{
+    var count = 0;
+
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            ++count;
+    }
+
+    return count;
+}
+
+function isUICleanable()
+{
+    return countProperties(selectedReferences) > 0 ||
+           countProperties(displayedVerses) > 0;
+}
+
+function toggleResetButton()
+{
+    resetButton.disabled = !isUICleanable();
+}
+
+function toggleLaunchButton()
+{
+    launchButton.disabled = !isFormSubmitable();
+}
+
+/**
+ * Gestion du protocole
+ */
+
+/* 
+ * Envoi une requête de versets
+ */
+
+function requestServer()
+{
+    if (!s) {
+        console.error("not connected to the WS server");
+        return;
+    }
+    var dict = {
+        "now": new Date().getTime(),
+        "ref": [],
+        "tra": filterForm.elements["choix_traduction"].value,
+        "cas": caseInsensitiveCheckbox.value == "on" ? true : false
+    };
+    var allWords   = filterForm.elements["conjonction"].value;
+    var oneOfWords = filterForm.elements["quelconque"].value;
+    var noneWords  = filterForm.elements["aucun"].value;
+    var numLow     = filterForm.elements["nombres_min"].value;
+    var numHigh    = filterForm.elements["nombres_max"].value;
+    if (allWords.length) {
+        dict["all"] = allWords.split(/\s+/).filter(ret);
+    }
+    if (oneOfWords.length) {
+        dict["one"] = oneOfWords.split(/\s+/).filter(ret);
+    }
+    if (noneWords.length) {
+        dict["non"] = noneWords.split(/\s+/).filter(ret);
+    }
+    if (numLow && numHigh) {
+        dict["ran"] = [numLow, numHigh];
+    }
+    for (var r in selectedReferences) {
+        dict["ref"].push(r);
+    }
+    // Envoi au serveur websocket
+    var jsonData = JSON.stringify(dict);
+    s.send(jsonData);
+}
+
+/*
+ * Construit une chaîne de requète URI à partir d'un blob
+ */
+
+function dictToQueryString(dict)
+{
+    var s="", e, first = true;
+    for (e in dict) {
+        if (!first) {
+            s += "&";
+        } else {
+            first = false;
+        }
+        s += e+"="+dict[e];
+    }
+    return s;
+}
+
+/**
+ * Gestion de la connection WebSocket
+ */
+
+var s;
+
+function triggerConnected(e)
+{
+    console.log("connected to the server: "+host+ress);
+}
+
+function handleMessage(e)
+{
+    if (typeof(e.data) == "object") return; // Blob
+    var resp = JSON.parse(e.data);
+    if (!("res" in resp) || !("now" in resp)) {
+        console.error("malformed JSON received");
+        return;
+    }
+    cleanDisplayedVerses();
+    for (var i=0, ref; i < resp["res"].length; ++i) {
+        ref = resp["res"][i];
+        displayVerse(ref["ref"], ref["verse"]);
+    }
+}
+
+function handleError(e)
+{
+    s.close();
+    s = null;
+};
+
+function triggerClosed(e)
+{
+    s = null;
+}
+
+function connectToServer()
+{
+    if (s && s.readyState == WebSocket.OPEN) {
+        s.close();
+    }
+    s = new WebSocket("ws://"+host+ress);	
+    s.addEventListener("open",    triggerConnected, false);
+    s.addEventListener("message", handleMessage, false);
+    s.addEventListener("error",   handleError, false);
+    s.addEventListener("close",   triggerClosed, false);
+    // bouton de recherche
+    s.addEventListener("open",  toggleLaunchButton, false);
+    s.addEventListener("close", toggleLaunchButton, false);
+    s.addEventListener("error", toggleLaunchButton, false);
+}
+
+/**
+ * Classe décrivant une référence biblique.
+ * Utiliser "parse" pour extraire les propriétés de la chaîne et "serialize"
+ * pour construire une référence propre.
+ */
+
+function Reference(referenceStr)
+{
+    this.referenceStr = referenceStr;
+    
+    this.book    = null;
+    this.chapter = null;
+    this.range   = {"low": null, "high": null};
+    
+    this.serialize = function() {
+        var ref = this.book;
+        if (this.chapter) {
+            ref += " " + this.chapter;
+        }
+        if (this.range["low"]) {
+            ref += "." + this.range["low"];
+            if (this.range["high"]) {
+                ref += "-" + this.range["high"];
+            }
+        }
+        return ref;
+    };
+    
+    this.filterWhiteStr = function(s)
+    {
+        return s.length > 0 && s.search(/^\s+$/) == -1;
+    }
+    
+    /*
+     * Obligatoire de trouver au moins le nom de chapitre
+     */
+
+    this.parse = function() {
+        var fragments = this.referenceStr.split(/(\s+|\b)/).filter(this.filterWhiteStr);
+        if (fragments.length < 1) return false;
+        // nom du livre
+        this.book = fragments[0];
+        // le premier élément est le numéro du livre
+        if (isNaN(fragments[0])) {
+            fragments = fragments.slice(1,fragments.length);
+        } else {
+            this.book += " " + fragments[1];
+            fragments = fragments.slice(2,fragments.length);
+        }
+        // vérification du nom du livre
+        if (bookIndex.indexOf(this.book) == -1) {
+            return false;
+        }
+        // chapitre
+        if (fragments.length == 0) {
+            return true;
+        } else if (isNaN(fragments[0])) {
+            return false;
+        } else {
+            this.chapter = fragments[0];
+            fragments = fragments.slice(1,fragments.length);
+        }
+        // point
+        if (fragments.length == 0) {
+            return true;
+        } else if (fragments[0] != ".") {
+            return false;
+        } else {
+            fragments = fragments.slice(1,fragments.length);
+        }
+        // indice haut
+        if (fragments.length == 0) {
+            return true;
+        } else if (isNaN(fragments[0])) {
+            return false;
+        } else {
+            this.range["low"] = fragments[0];
+            fragments = fragments.slice(1,fragments.length);
+        }
+        // tiret
+        if (fragments.length == 0) {
+            return true;
+        } else if (fragments[0] != "-") {
+            return false;
+        } else {
+            fragments = fragments.slice(1,fragments.length);
+        }
+        // indice bas
+        if (fragments.length == 0) {
+            return false;
+        } else if (isNaN(fragments[0])) {
+            return false;
+        } else {
+            this.range["high"] = fragments[0];
+        }
+        return true;
+    };
+}
---
+++ lib/BibleParser/XMLBibleParser.py
@@ -0,0 +1,286 @@
+import re
+
+from xml.etree.ElementTree import ElementTree, Element
+
+class XMLBibleParser:
+    """
+    Un itérateur sur des versets de la bible.
+    Permet de rechercher par références et par mots-clés simultanément.
+    """
+
+    # Masque d'extraction des éléments composant une référence
+    _regex_extract_reference = re.compile(
+        """^"""
+        """(?P<book>([123] )?\w+)"""
+        """( (((?P<chapter_low>\d+)(-(?P<chapter_high>\d+))?)"""
+        """|(?P<no_chapter_index>\*))"""
+        """(\.((?P<verse_low>\d+)(-(?P<verse_high>\d+))?"""
+        """|(?P<no_verse_index>\*)))?)?"""
+        """$"""
+    )
+
+    mandatory_keywords = []
+    one_of_keywords    = []
+    none_of_keywords   = []
+    number_ranges      = []
+    
+    references = {}
+    
+    case_insensitive = False
+    
+    class ReferenceError(ValueError):
+        pass
+    
+    def __init__(self, xml, lexicon=None):
+        if isinstance(xml, Element):
+            self.bible = xml
+        elif isinstance(xml, str):
+            tree = ElementTree()
+            self.bible = tree.parse(xml)
+        else:
+            raise ValueError("'xml' argument is not an 'ElementTree' instance nor a path to a XML file")
+        if lexicon is not None and not isinstance(lexicon, LexicalContext):
+            raise ValueError("'lexicon' parameter is not a 'LexicalContext' instance")
+        self.lexicon = lexicon
+
+    def add_reference(self, ref):
+        attr = self.parse_reference(ref)
+        self.references[ref] = attr
+
+    def parse_reference(self, reference):
+        """
+        Extrait les attributs d'une référence biblique sous la forme d'un
+        dictionnaire.
+        """
+        match = XMLBibleParser._regex_extract_reference.match(reference)
+        if match is None:
+            raise BibleParser.ReferenceError(
+                'invalid reference "{}"'.format(reference)
+            )
+        attribs = match.groupdict()
+        # Extraction des attributs du livre
+        if attribs['book'] is None:
+            raise BibleParser.ReferenceError("no book given")
+        # Extraction des attributs des chapitres
+        if attribs['chapter_low'] is None:
+            if attribs['no_chapter_index'] is not None:
+                attribs['chapter_low'] = -1
+            # Permet une sélection globale par omission à condition de ne
+            # pas spécifier plus précis quand on ommet moins précis
+            else:
+                if attribs['verse_low'] is None:
+                    attribs['chapter_low'] = -1
+                else:
+                    raise BibleParser.ReferenceError("no chapter given")
+        else:
+            attribs['chapter_low'] = int(attribs['chapter_low'])
+            if attribs['chapter_high'] is not None:
+                attribs['chapter_high'] = int(attribs['chapter_high'])
+                if attribs['chapter_low'] >= attribs['chapter_high']:
+                    raise BibleParser.ReferenceError(
+                        "invalid chapter range"
+                    )
+            else:
+                attribs['chapter_high'] = -1
+        # Extraction des attributs des versets
+        if attribs['verse_low'] is None:
+                attribs['verse_low'] = -1
+        else:
+            attribs['verse_low'] = int(attribs['verse_low'])
+            if attribs['verse_high'] is not None:
+                attribs['verse_high'] = int(attribs['verse_high'])
+                if attribs['verse_low'] >= attribs['verse_high']:
+                    raise BibleParser.ReferenceError("invalid verse range")
+            else:
+                attribs['verse_high'] = -1
+        return attribs
+
+    def get_greatest_element_index(self, root, element):
+        """
+        Retourne le plus grand index (attribut "n") des sous-éléments du
+        noeud racine /root/.
+        Les éléments sont de type /element/.
+        """
+        greatest = None
+        for child in root.iterfind("./{}".format(element)):
+            greatest = child
+        if greatest is None:
+            return greatest
+        return int(greatest.attrib["n"])
+
+    def verse_match_rules(self, verse):
+        """
+        Cherche à reconnaitre au moins un mot-clé dans le verset donné en
+        argument.
+        """
+        # Mots étants _tous_ obligatoires
+        for r in self.mandatory_keywords:
+            if not r.search(verse):
+                return False
+        # Mots dont au moins un est nécessaire
+        if len(self.one_of_keywords) > 0:
+            one_found = False
+            for r in self.one_of_keywords:
+                if r.search(verse):
+                    one_found = True
+                    break
+            if not one_found:
+                return False
+        # Mots interdits
+        for r in self.none_of_keywords:
+            if r.search(verse):
+                return False
+        return True
+    
+    def _compile_keyword_regex(self, s):
+        """
+        Compile une expression régulière détectant un mot délimité, avec ou sans
+        sensibilité à la case.
+        """
+        if self.case_insensitive:
+            return re.compile(s, re.I)
+        else:
+            return re.compile(s)
+    
+    def add_mandatory_keywords(self, words):
+        """
+        Ajoute à la liste des mots-clés tous obligatoires.
+        """
+        self.mandatory_keywords.extend([
+            self._compile_keyword_regex(x) for x in words
+        ])
+
+    def add_one_of_keywords(self, words):
+        """
+        Ajoute à la liste des mots-clés dont _un seul_ est obligatoire.
+        """
+        self.one_of_keywords.extend([
+            self._compile_keyword_regex(x) for x in words
+        ])
+
+    def add_none_of_keywords(self, words):
+        """
+        Ajoute à la liste noire des mots-clés.
+        """
+        self.none_of_keywords.extend([
+            self._compile_keyword_regex(x) for x in words
+        ])
+    
+    def add_number_in_range(self, ran):
+        """
+        Ajoute à la liste un intervalle de nombres à détecter.
+        """
+        l, h = int(ran["low"]), int(ran["high"])
+        if h > l:
+            raise ValueError("the range is not valid")
+        self.number_ranges.append((l,h))
+    
+    def set_case_insensitivity(self, insensitive):
+        """
+        Active ou non l'insensibilité à la case.
+        """
+        self.case_insensitive = insensitive
+    
+    def clean(self):
+        """
+        Rétablit le parseur dans son état initial.
+        """
+        self.references.clear()
+        self.mandatory_keywords.clear()
+        self.one_of_keywords.clear()
+        self.none_of_keywords.clear()
+        self.number_ranges.clear()
+    
+    def __iter__(self):
+        """
+        Recherche dans la bible à partir de références et les retournes une 
+        à une sous la forme de chaînes préfixées par leur référence.
+        """
+        # Parcours toute la bible en cas d'absence de référence
+        if not self.references:
+            for book in self.bible.iterfind("./b"):
+                for chapter in book.iterfind("./c"):
+                    for verse in chapter.iterfind("./v"):
+                        if verse.text is None:
+                            continue
+                        # Barrière de concordance avec les mots-clés
+                        if not self.verse_match_rules(verse.text):
+                            continue
+                        # Mise-à-jour du lexique
+                        if self.lexicon:
+                            self.lexicon.update(verse.text)
+                        else:
+                            yield (book.attrib["n"],
+                                   int(chapter.attrib["n"]),
+                                   int(verse.attrib["n"]),
+                                   verse.text)
+        # Parcours uniquement des références précises
+        else:
+            for reference in self.references:
+                attribs = self.parse_reference(reference)
+                book = self.bible.find('./b[@n="{}"]'.format(attribs['book']))
+                if book is None:
+                    raise BibleParser.ReferenceError(
+                        'invalid book name "{}"'.format(attribs['book'])
+                    )
+                # Sélectionne tous les chapitres
+                if attribs['chapter_low'] == -1:
+                    chapter_range = range(
+                        1,
+                        self.get_greatest_element_index(book, 'c')+1
+                    )
+                # Sélectionne un intervalle de chapitres
+                elif attribs['chapter_high'] != -1:
+                    chapter_range = range(
+                        attribs['chapter_low'],
+                        attribs['chapter_high']+1
+                    )
+                # Sélectionne un seul chapitre
+                else:
+                    chapter_range = (attribs['chapter_low'],)
+                for chapter_index in chapter_range:
+                    chapter = book.find('./c[@n="{}"]'.format(
+                        chapter_index
+                    ))
+                    if chapter is None:
+                        raise BibleParser.ReferenceError(
+                            'invalid chapter number "{}"'.format(
+                                chapter_index
+                            )
+                        )
+                    # Sélectionne tous les versets du chapitre
+                    if attribs['verse_low'] == -1:
+                        verse_range = range(
+                            1,
+                            self.get_greatest_element_index(chapter, 'v')+1
+                        )
+                    # Sélectionne un intervalle de versets
+                    elif attribs['verse_high'] != -1:
+                        verse_range = range(
+                            attribs['verse_low'],
+                            attribs['verse_high']+1
+                        )
+                    # Sélectionne un seul verset
+                    else:
+                        verse_range = (attribs['verse_low'],)
+                    for verse_index in verse_range:
+                        verse = chapter.find('./v[@n="{}"]'.format(
+                            verse_index
+                        ))
+                        if verse is None:
+                            raise BibleParser.ReferenceError(
+                                'invalid verse index "{}"'.format(
+                                    verse_index
+                                )
+                            )
+                        # Barrière de concordance avec les mots-clés
+                        if not self.verse_match_rules(verse.text):
+                            continue
+                        # Mise-à-jour du lexique
+                        if self.lexicon:
+                            self.lexicon.update(verse.text)
+                        else:
+                            yield (book.attrib["n"],
+                                   int(chapter.attrib["n"]),
+                                   int(verse.attrib["n"]),
+                                   verse.text)
