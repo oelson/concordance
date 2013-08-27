0a29d62e0a23f87d1e9f185d15e5c97007365b26
Nelson HOUILLON <houillon.nelson@gmail.com>
2013-08-27 18:49:15 +0200

    sauvegarde de l'état de l'interface

--- client/client.js
+++ client/client.js
@@ -89,6 +89,7 @@ function init()
     reinitButton.addEventListener("click", cleanDisplayedVerses, false);
     suggestionCloseImg.addEventListener("click", hideBookSuggestion, false);
     // WS
+    restoreFormState();
     connectToServer();
 }
 
@@ -223,6 +224,7 @@ function cleanForm()
     cleanReferenceList();
     clearBookSuggestion();
     filterForm.reset();
+    saveFormState();
 }
 
 /*
@@ -368,31 +370,64 @@ function signalReferenceError()
     referenceErrorSpan.classList.add("error");
 }
 
-/*
- * Soumission du formulaire
+/**
+ * Persistence des données saisies et récupérées
  */
 
-function isFormSubmitable()
+function saveFormState()
 {
-    if (s && s.readyState == WebSocket.OPEN) {
-        return true;
+    localStorage["tra"] = filterForm.elements["traduction"].value;
+    localStorage["all"] = filterForm.elements["conjonction"].value;
+    localStorage["one"] = filterForm.elements["quelconque"].value;
+    localStorage["non"] = filterForm.elements["aucun"].value;
+    localStorage["cas"] = filterForm.elements["case"].checked;
+    localStorage["acc"] = filterForm.elements["accent"].checked;
+    localStorage["bou"] = filterForm.elements["mot"].checked;
+    localStorage["ran"] = filterForm.elements["nombres_min"].value + ","
+                        + filterForm.elements["nombres_max"].value;
+    var refs = [];
+    for (var r in selectedReferences) {
+        refs.push(r);
     }
-    return false;
+    localStorage["ref"] = refs;
 }
 
-function isUICleanable()
+function restoreFormState()
 {
-    return resultTable.tBodies[0].childElementCount > 1;
-}
-
-function toggleResetButton()
-{
-    resetButton.disabled = !isUICleanable();
-}
-
-function toggleLaunchButton()
-{
-    launchButton.disabled = !isFormSubmitable();
+    if ("tra" in localStorage) {
+        filterForm.elements["traduction"].value = localStorage["tra"];
+    }
+    if ("all" in localStorage) {
+        filterForm.elements["conjonction"].value = localStorage["all"];
+    }
+    if ("one" in localStorage) {
+        filterForm.elements["quelconque"].value = localStorage["one"];
+    }
+    if ("non" in localStorage) {
+        filterForm.elements["aucun"].value = localStorage["non"];
+    }
+    if ("cas" in localStorage) {
+        filterForm.elements["case"].checked = (localStorage["cas"] == "true");
+    }
+    if ("acc" in localStorage) {
+        filterForm.elements["accent"].checked = (localStorage["acc"] == "true");
+    }
+    if ("bou" in localStorage) {
+        filterForm.elements["mot"].checked = (localStorage["bou"] == true);
+    }
+    if ("ran" in localStorage) {
+        var range = localStorage["ran"].split(",");
+        filterForm.elements["nombres_min"].value = range[0];
+        filterForm.elements["nombres_max"].value = range[1];
+    }
+    if ("ref" in localStorage) {
+        var refs = localStorage["ref"].split(",");
+        for (var i=0, s, reference; i < refs.length; ++i) {
+            s = refs[i];
+            addReference(s);
+        }
+    }
+    toggleCleanButton();
 }
 
 /**
@@ -408,6 +443,7 @@ function requestServer()
     if (!s) {
         return;
     }
+    saveFormState();
     var dict = {
         "now": new Date().getTime(),
         "ref": [],
